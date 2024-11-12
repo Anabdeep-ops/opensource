773289f484cdc61b894b933c325497db7183664d
AmanDeepSingh <140300034+Anabdeep-ops@users.noreply.github.com>
2024-11-12 10:31:04 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.io*;
+import java.util.*;
+class solution{
+  public static void main(string[] args){
+  Scanner sc=new Scanner(System.in);
+  int n= sc.nextInt();
+  if(n%2==0){
+  System.out.println("Even");
+}else{
+  System.out.println("Odd");
+}
+  }
+}
+  
